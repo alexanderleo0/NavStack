f56e541f5a39dc82a68e951a86873abcd91812b8
alexanderleo0 <alnikitin3@gmail.com>
2023-03-14 10:52:55 +0300

    1

--- Tests/NavStackTests/NavStackTests.swift
+++ Tests/NavStackTests/NavStackTests.swift
@@ -1,11 +1,11 @@
-import XCTest
-@testable import NavStack
-
-final class NavStackTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(NavStack().text, "Hello, World!")
-    }
-}
+//import XCTest
+//@testable import NavStack
+//
+//final class NavStackTests: XCTestCase {
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+//        XCTAssertEqual(NavStack().text, "Hello, World!")
+//    }
+//}
